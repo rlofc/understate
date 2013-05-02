1e463a7ab152548df00fbc5ddf51aaab433fe108
Ithai Levi <ithai.levi@gmail.com>
2013-05-02 20:33:44 +0300

    basic test runner

---
+++ Makefile
@@ -0,0 +1,3 @@
+test:
+	python -m unittest discover -p '*_test.py'
+
---
+++ tests/parser_test.py
@@ -0,0 +1,9 @@
+import unittest
+from understate.parser import *
+
+class ParserTest(unittest.TestCase):
+    def test_tests(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+        unittest.main()
